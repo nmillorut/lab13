ab155dc6a0cfc640eb9bc81435fd3ed9aa10be90
nmillorut <nmillorut@gmail.com>
2024-12-30 16:31:44 +0700

    lab13_2.cpp completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,29 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+void showImage(const bool image[][M]){
+    cout<<"------------------------------------------------------------------------";
+    for(int i=0;i<30;++i){   
+        cout<<endl;
+        cout<<"|";
+        for(int j=0;j<70;++j){
+            if (image[i][j] == 1) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        cout<<"|";
+    }
+    cout<<"\n------------------------------------------------------------------------\n";
+}
+void updateImage(bool Image[][M],int s,int x,int y){
+     for(int i=0;i<30;++i){   
+        for(int j=0;j<70;++j){
+            if (sqrt(pow(i - x, 2) + pow(j - y, 2)) <= s-1) {
+                    Image[i][j] = true;
+                }
+            if(s==0 && x==0 && y==0){break;}
+        }
+    }
+}
